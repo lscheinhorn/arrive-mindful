e512f4fd0aafffc41f006a01c248ef8b72e56cc0
Jennifer Scheinhorn <jscheinhorn@gmail.com>
2021-05-07 11:28:17 -0400

    feat: run from server, no proxy

--- src/server/index.js
+++ src/server/index.js
@@ -58,7 +58,7 @@ app.post('/email', async function (req, res) {
   }
 });
 
-app.get('/', function (req, res) {
+app.get('*', function (req, res) {
   res.sendFile(path.join(__dirname, '..','..', 'build', 'index.html'));
 });
 
