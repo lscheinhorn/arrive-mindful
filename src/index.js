45acbc84410e4baa9784cfc07378ea3fe6cd33c2
lscheinhorn <lukescheinhorn@gmail.com>
2023-06-25 15:57:58 -0400

    fix: router working

--- src/index.js
+++ src/index.js
@@ -4,44 +4,50 @@ import './css/style.css'
 import './css/flaticon.css';
 import './css/icomoon.css';
 import { Navbar, Footer, Home, Gallery, About, Classes, Schedule, Blog, Contact } from './components';
-import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <Router>
       <Navbar />
-      <Switch>
-        <Route path="arrive-mindful/home">
-          <Home />
-        </Route>
-        <Route path="arrive-mindful/gallery">
-          <Gallery banner={true} />
-        </Route>
-        <Route path="arrive-mindful/about">
-          <About />
-        </Route>
-        <Route path="arrive-mindful/classes">
-          <Classes />
-        </Route>
-        <Route path="arrive-mindful/schedule">
-          <Schedule range="month" banner={true} />
-        </Route>
-        <Route path="arrive-mindful/blog">
-          <Blog />
-        </Route>
-        <Route path="arrive-mindful/contact">
-          <Contact />
-        </Route>
-        <Route exact path="arrive-mindful/">
-          <Home />
-        </Route>
-      </Switch>
+      <Routes>
+        <Route 
+          path="/home"
+          element={<Home />}
+        />
+        <Route 
+          path="/gallery"
+          element={<Gallery banner={true} />}
+        />
+        <Route 
+          path="/about"
+          element={<About />}
+        />
+        <Route 
+          path="/classes"
+          element={<Classes />}
+        />
+        <Route 
+          path="/schedule"
+          element={<Schedule range="month" banner={true} />}
+        />
+        <Route 
+          path="/blog"
+          element={<Blog />}
+        />
+        <Route 
+          path="/contact"
+          element={<Contact />}
+        />
+        <Route 
+          exact path="/"
+          element={<Home />}
+        />
+      </Routes>
       <Footer />
-    </Router>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    </Router>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
